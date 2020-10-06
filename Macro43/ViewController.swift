3b5ffd8b69a1f79a63c391ed4df84fad4789c041
Toriq Wahid Syaefullah <toriqwahidsyaef@gmail.com>
2020-10-06 12:05:12 +0700

    Initial Commit

---
+++ Macro43/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Macro43
+//
+//  Created by Toriq Wahid Syaefullah on 06/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
