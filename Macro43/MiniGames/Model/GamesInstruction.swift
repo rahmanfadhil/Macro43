5323038554c8b90dab9ee358b52e0a932cb5ae52
Setiawan Joddy <37212064+setiawanjoddy@users.noreply.github.com>
2020-10-07 12:27:52 +0700

    Games Instruction Added

---
+++ Macro43/MiniGames/Model/GamesInstruction.swift
@@ -0,0 +1,22 @@
+//
+//  GamesInstruction.swift
+//  Macro43
+//
+//  Created by Setiawan Joddy on 07/10/20.
+//
+
+import Foundation
+
+struct GamesInstruction {
+    var gameTitleName: String
+    var instructionContent: String
+    var activityHelpsContent: String
+    var basedOnResearch: String
+}
+
+var gamesInstructionArray =
+    [GamesInstruction(gameTitleName: "Express Emotion", instructionContent: "Ini Instruksi Bakal EE", activityHelpsContent: "Pokoknya Ini akan membantumu!", basedOnResearch: "Aduh, Bagus Pokoknya"),
+    GamesInstruction(gameTitleName: "Share Story", instructionContent: "Ayodong Cerita", activityHelpsContent: "Dijamin 100% Makin PD", basedOnResearch: "Terpercaya"),
+    GamesInstruction(gameTitleName: "Smash It", instructionContent: "Tabok aja bor", activityHelpsContent: "Bikin emosimu hilang", basedOnResearch: "Semangatt"),
+    GamesInstruction(gameTitleName: "Breathing", instructionContent: "Napas aja sih", activityHelpsContent: "Dijamin panjang umur", basedOnResearch: "Skuyyy"),
+    GamesInstruction(gameTitleName: "Pop Bubble", instructionContent: "Pecahkan gelembung", activityHelpsContent: "Yaudah si tinggal pecahin", basedOnResearch: "Bikin Candu")]
