1fd4631b577052057a8082ef14ecb7954613f4fe
Rahman Fadhil <rhmnfadhil@gmail.com>
2020-10-08 12:44:36 +0700

    Add AlertHelper

---
+++ Macro43/Helpers/AlertHelper.swift
@@ -0,0 +1,30 @@
+//
+//  AlertHelper.swift
+//  Macro43
+//
+//  Created by Rahman Fadhil on 08/10/20.
+//
+
+import Foundation
+import UIKit
+
+class AlertHelper {
+    static func createAlert(title: String, message: String, onComplete: @escaping () -> Void) -> UIAlertController {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: { (action) in
+            onComplete()
+        }))
+        return alert
+    }
+    
+    static func createPrompt(title: String, question: String, onComplete: @escaping (Bool) -> Void) -> UIAlertController {
+        let alert = UIAlertController(title: title, message: question, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in
+            onComplete(false)
+        }))
+        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { (action) in
+            onComplete(true)
+        }))
+        return alert
+    }
+}
