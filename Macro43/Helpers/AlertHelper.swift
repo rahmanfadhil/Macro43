5a3d1524dd8f0cb619c90b3b2577d83cee3b591a
Rahman Fadhil <rhmnfadhil@gmail.com>
2020-10-08 12:47:17 +0700

    Add comments to AlertHelper methods

--- Macro43/Helpers/AlertHelper.swift
+++ Macro43/Helpers/AlertHelper.swift
@@ -9,6 +9,7 @@ import Foundation
 import UIKit
 
 class AlertHelper {
+    // Create an alert with "Okay" button.
     static func createAlert(title: String, message: String, onComplete: @escaping () -> Void) -> UIAlertController {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: { (action) in
@@ -17,6 +18,7 @@ class AlertHelper {
         return alert
     }
     
+    // Create a questionnaire alert with "Yes" and "No" button.
     static func createPrompt(title: String, question: String, onComplete: @escaping (Bool) -> Void) -> UIAlertController {
         let alert = UIAlertController(title: title, message: question, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in
